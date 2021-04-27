4086af7f47f4ce33c9e53f223d196a69e4351192
Giang Do <dhtgiang145@gmail.com>
2021-04-26 21:07:13 -0700

    v1.4

--- Views/active.js
+++ Views/active.js
@@ -1,7 +1,18 @@
-$(".nav-item").click(function () {
-  var navlist = $(".nav-item");
-  for (let i = 0; i < navlist.length; i++) {
-    navlist[i].classList.remove("active");
-  }
-  this.classList.add("active");
-});
+// $(document).ready(function () {
+//   $(".nav-item").click(function () {
+//     var navlist = $(".nav-item");
+//     for (let i = 0; i < navlist.length; i++) {
+//       navlist[i].classList.remove("active");
+//     }
+//     this.classList.add("active");
+//   });
+// });
+
+// var navItems = document.getElementsByClassName("nav-item");
+// for (var i = 0; i < navItems.length; i++) {
+//   navItems[i].addEventListener("click", function () {
+//     var current = document.getElementsByClassName("active");
+//     current[0].className = current[0].className.replace(" active", "");
+//     this.className += " active";
+//   });
+// }
